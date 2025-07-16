52128536d49102d7994dd4486013a8868fcecd09
Indrajams2006 <indrajams2028@cs.sjcetpalai.ac.in>
2025-07-16 10:07:06 +0530

    Add files via upload

---
+++ Hellojava.java
@@ -0,0 +1,8 @@
+package indraja;
+
+public class Hellojava {
+ public static void main (String [] args ) {
+	 System.out.println("Hello Java!");
+ }
+ }
+
---
+++ Userjava.java
@@ -0,0 +1,10 @@
+package indraja;
+import java.util.Scanner;
+public class Userjava {
+	public static void main(String [] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int number=sc.nextInt();
+		System.out.println("The number:"+number);
+	}
+}
